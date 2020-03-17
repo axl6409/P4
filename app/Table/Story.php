9b7b10d9b2fa2685b57257312032f16650ecb369
axl6409 <alexandre.celier64@gmail.com>
2020-03-17 12:22:15 +0100

    New magic function __get & function for the excerpt

--- app/Table/Story.php
+++ app/Table/Story.php
@@ -4,13 +4,21 @@ namespace App\Table;
 
 class Story {
 
+    public function __get($key) {
+        $method = 'get' . ucfirst($key);
+        $this->$key = $this->$method();
+        return $this->$key;
+    }
+
     public function getURL() {
 
         return 'index.php?p=story$id=' . $this->id;
     }
 
     public function getExcerpt() {
-        return $this->content;
+        $html = '<p>' . substr($this->content, 0, 250) . '...</p>';
+        $html .= '<p><a href="' . $this->getURL() . '">Voir la suite</a></p>';
+        return $html;
     }
 
 }
