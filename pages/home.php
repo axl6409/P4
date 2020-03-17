32370c40dd07cd67158b8125a09126d5f7798d2b
axl6409 <alexandre.celier64@gmail.com>
2020-03-17 12:11:20 +0100

    Include new static class getDb of App.php

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,11 @@
 
 
-<?php foreach($db->query('SELECT * FROM stories', 'App\Table\Story') as $story) : ?>
+<?php foreach(App\App::getDb()->query('SELECT * FROM stories', 'App\Table\Story') as $story) : ?>
 
 
-    <h2><a href="<?= $story->getURL(); ?>"><?= $story->title; ?></a></h2>
+    <h2><a href="<?= $story->url; ?>"><?= $story->title; ?></a></h2>
 
-    <p><?= $story->getExcerpt(); ?></p>
+    <p><?= $story->excerpt; ?></p>
 
 
 <?php endforeach; ?>
