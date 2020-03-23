68b3868cd6614c12344f88461e22e7ee6ee3a6da
axl6409 <alexandre.celier64@gmail.com>
2020-03-23 12:44:49 +0100

    Static class | Return db config results of config/config.php file

---
+++ app/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Config {
+
+    private $settings = [];
+
+    public function __construct() {
+
+        $this->settings = require dirname(__DIR__) . '/config/config.php';
+
+    }
+
+}
