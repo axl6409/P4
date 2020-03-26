95a6f9066f8d46fa6e41935be41e326bb309424c
axl6409 <alexandre.celier64@gmail.com>
2020-03-26 10:37:08 +0100

    Login function in UsersController | Login page check connected

--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -1,25 +1,8 @@
-<?php
-
-use Core\HTML\BootstrapForm;
-use Core\Auth\DBAuth;
-
-if (!empty($_POST)) {
-    $auth = new DBAuth(App::getInstance()->getDb());
-    if ($auth->login($_POST['username'], $_POST['password'])) {
-        header('Location: admin.php');
-    } else {
-        ?>
-        <div class="alert alert-danger">
-            Identifiants Incorrect
-        </div>
-        <?php
-    }
-}
-
-$form = new BootstrapForm($_POST);
-
-?>
-
+<?php if ($errors): ?>
+    <div class="alert alert-danger">
+        Identifiants Incorrects
+    </div>
+<?php endif; ?>
 
 <form method="post">
     <?= $form->input('username', 'pseudo'); ?>
