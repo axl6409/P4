a9822d13e3014a8dc0ed5454fea45e8fad2449d7
axl6409 <alexandre.celier64@gmail.com>
2020-03-26 10:48:17 +0100

    Chnages add, edit, delete functions into the Controller

--- app/Controller/Admin/PostsController.php
+++ app/Controller/Admin/PostsController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller\Admin;
 
+use Core\HTML\BootstrapForm;
 
 class PostsController extends AppController {
 
@@ -14,6 +15,51 @@ class PostsController extends AppController {
     public function index() {
         $posts = $this->Post->all();
         $this->render('admin.posts.index', compact('posts'));
+    }
+
+    public function add() {
+
+        if (!empty($_POST)) {
+            $result = $this->Post->create([
+                'title'     => $_POST['title'],
+                'content'   => $_POST['content']
+            ]);
+
+            if ($result) {
+                return $this->index();
+            }
+        }
+
+        $form = new BootstrapForm($_POST);
+        $this->render('admin.posts.edit');
+    }
+
+    public function edit() {
+
+        if (!empty($_POST)) {
+            $result = $this->Post->update($_GET['id'], [
+                'title'     => $_POST['title'],
+                'content'   => $_POST['content']
+            ]);
+
+            if ($result) {
+               return $this->index();
+            }
+        }
+
+        $post = $this->Post->find($_GET['id']);
+        $form = new BootstrapForm($post);
+        $this->render('admin.posts.edit');
+    }
+
+    public function delete() {
+
+        if (!empty($_POST)) {
+            $result = $this->Post->delete($_POST['id']);
+            return $this->index();
+
+        }
+
     }
 
 }
--- app/Views/admin/posts/edit.php
+++ app/Views/admin/posts/edit.php
@@ -1,26 +1,5 @@
 <?php
 
-use Core\HTML\BootstrapForm;
-use Core\Auth\DBAuth;
-
-$postTable = App::getInstance()->getTable('Post');
-
-if (!empty($_POST)) {
-    $result = $postTable->update($_GET['id'], [
-            'title'     => $_POST['title'],
-            'content'   => $_POST['content']
-    ]);
-
-    if ($result) {
-        ?>
-        <div class="alert alert-success">L'article à bien été mis à jour</div>
-        <?php
-    }
-}
-
-$post = $postTable->find($_GET['id']);
-$form = new BootstrapForm($post);
-
 ?>
 
 
