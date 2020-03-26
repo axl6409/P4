d0339ba8b8da637bd4b85295c79bc93053d05dfb
axl6409 <alexandre.celier64@gmail.com>
2020-03-26 17:57:32 +0100

    Fragment the default template & prepare for SASS

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,9 @@
+        </main><!-- /.container -->
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <!-- Include the Quill library -->
+        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+        <!-- Initialize Quill editor -->
+        <script src="js/squill-editor.js"></script>
+    </body>
+</html>
