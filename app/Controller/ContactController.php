8e7ea1a805cfb6ac9c0d8bec575f9cf202450527
axl6409 <alexandre.celier64@gmail.com>
2020-04-23 11:09:10 +0200

    Secure the contact form

--- app/Controller/ContactController.php
+++ app/Controller/ContactController.php
@@ -79,39 +79,44 @@ class ContactController extends AppController {
 
             //... The Captcha is valid you can continue with the rest of your code
             //... Add code to filter access using $response . score
-            if ($response->success == true && $response->score <= 0.5) {
-                $errors['captcha'] = "Vous n'êtes pas un Robot !";
-            }
+            if ($response->success == true && $response->score <= 0.9) {
 
-            if (!array_key_exists('name', $_POST) || $_POST['name'] == '') {
-                $errors['name'] = "Vous n'avez pas renseigné votre nom";
-            }
-            if (!array_key_exists('firstname', $_POST) || $_POST['firstname'] == '') {
-                $errors['firstname'] = "Vous n'avez pas renseigné votre firstname";
-            }
-            if (!array_key_exists('mail', $_POST) || $_POST['mail'] == '') {
-                $errors['mail'] = "Vous n'avez pas renseigné votre email";
-            } elseif(filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) == false) {
-                $errors['mail'] = "Vous n'avez pas renseigné un email valide";
-            }
-            if (!array_key_exists('message', $_POST) || $_POST['message'] == '') {
-                $errors['message'] = "Vous n'avez pas renseigné votre message";
-            }
+                if (!preg_match("/^[a-zA-Z]*$/", $_POST['name'])) {
+                    $errors['name'] = "Uniquement des lettres sont acceptés pour le champ nom";
+                }
+                if (!array_key_exists('name', $_POST) || $_POST['name'] == '') {
+                    $errors['name'] = "Vous n'avez pas renseigné votre nom";
+                }
+                if (!preg_match("/^[a-zA-Z]*$/", $_POST['firstname'])) {
+                    $errors['firstname'] = "Uniquement des lettres sont acceptés pour le champ prénom";
+                }
+                if (!array_key_exists('firstname', $_POST) || $_POST['firstname'] == '') {
+                    $errors['firstname'] = "Vous n'avez pas renseigné votre firstname";
+                }
+                if (!array_key_exists('mail', $_POST) || $_POST['mail'] == '') {
+                    $errors['mail'] = "Vous n'avez pas renseigné votre email";
+                } elseif(filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) == false) {
+                    $errors['mail'] = "Vous n'avez pas renseigné un email valide";
+                }
+                if (!array_key_exists('message', $_POST) || $_POST['message'] == '') {
+                    $errors['message'] = "Vous n'avez pas renseigné votre message";
+                }
 
-            if (!empty($errors)) {
-                $_SESSION['errors'] = $errors;
-                header('Location: index.php?p=contact.index');
-            } else {
-                $to      = $users->mail;
-                $subject = 'Contact depuis le site JeanForteroche';
-                $message = $_POST['message'];
-                $headers = 'From: '.$_POST['mail'].'' . "\r\n" .
-                    'Reply-To: '.$_POST['mail'].'' . "\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
-                mail($to, $subject, $message, $headers);
-                $errors['success'] = "Votre message à bien été envoyé ! Merci";
-                $_SESSION['errors'] = $errors;
-                header('Location: index.php?p=contact.index');
+                if (!empty($errors)) {
+                    $_SESSION['errors'] = $errors;
+                    header('Location: index.php?p=contact.index');
+                } else {
+                    $to      = $users->mail;
+                    $subject = 'Contact depuis le site JeanForteroche';
+                    $message = $_POST['message'];
+                    $headers = 'From: '.$_POST['mail'].'' . "\r\n" .
+                        'Reply-To: '.$_POST['mail'].'' . "\r\n" .
+                        'X-Mailer: PHP/' . phpversion();
+                    mail($to, $subject, $message, $headers);
+                    $errors['success'] = "Votre message à bien été envoyé ! Merci";
+                    $_SESSION['errors'] = $errors;
+                    header('Location: index.php?p=contact.index');
+                }
             }
 
         }
